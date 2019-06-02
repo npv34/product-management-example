9307172cc94eb178b03ae71101061ee6821fac1f
npv34 <phanvanluan611996@gmail.com>
2019-06-02 23:27:23 +0700

    show list product

--- src/controller/ProductController.php
+++ src/controller/ProductController.php
@@ -9,12 +9,20 @@ class ProductController
     private $productDB;
     public function __construct()
     {
-        $database = new DBConnect("mysql:host=localhost;dbname=product_manangerment", "root", "123456@Abc");
+        $database = new DBConnect("mysql:host=localhost;dbname=demo-shopping-cart", "root", "123456@Abc");
         $this->productDB = new ProductDB($database->connect());
     }
 
     public function index()
     {
+        $products = $this->productDB->getAll();
+        include "src/view/list.php";
     }
+
+    public function add(){}
+
+    public function edit(){}
+
+    public function delete(){}
 
 }
--- src/model/Product.php
+++ src/model/Product.php
@@ -3,11 +3,17 @@ namespace Model;
 
 class Product
 {
-    private $id;
-    private $name;
+    public $id;
+    public $name;
+    public $image;
+    public $price;
+    public $priceOld;
 
-    public function __construct($name)
+    public function __construct($name, $image, $price, $priceOld)
     {
         $this->name = $name;
+        $this->image = $image;
+        $this->price = $price;
+        $this->priceOld = $priceOld;
     }
 }
